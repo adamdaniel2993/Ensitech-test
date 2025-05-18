a64b71a8e4c4e7170f1501d8db7908fea54a9096
adan_daniel <adan@joinheard.com>
2025-05-18 15:33:22 -0400

    test

--- CICD/vpc.tf
+++ CICD/vpc.tf
@@ -9,7 +9,7 @@ module "vpc" {
   name = "${var.tenant}-${var.environment}-vpc"
   cidr = var.vpc_cird_block
 
-  azs             = ["us-east-2a", "us-east-2b"]
+  azs             = ["us-east-1a", "us-east-1b"]
   private_subnets = [local.vpc_subnets[0], local.vpc_subnets[1]]
   public_subnets  = [local.vpc_subnets[2], local.vpc_subnets[3]]
 
